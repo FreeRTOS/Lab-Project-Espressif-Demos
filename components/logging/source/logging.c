bc7fd2251a118c4c175e65769ff042cef978cb32
Gaurav Aggarwal <aggarg@amazon.com>
2022-01-15 20:43:10 -0800

    Add new line after each logging message
    This improves readability of logs.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- components/logging/source/logging.c
+++ components/logging/source/logging.c
@@ -5,6 +5,7 @@ void app_esp_log_debug_write(const char *format, ...)
     va_list list;
     va_start(list, format);
     esp_log_writev(ESP_LOG_DEBUG, "tag", format, list);
+    esp_log_write(ESP_LOG_DEBUG, "tag", "\r\n");
     va_end(list);
 }
 
@@ -13,6 +14,7 @@ void app_esp_log_info_write(const char *format, ...)
     va_list list;
     va_start(list, format);
     esp_log_writev(ESP_LOG_INFO, "tag", format, list);
+    esp_log_write(ESP_LOG_INFO, "tag", "\r\n");
     va_end(list);
 }
 
@@ -21,6 +23,7 @@ void app_esp_log_warn_write(const char *format, ...)
     va_list list;
     va_start(list, format);
     esp_log_writev(ESP_LOG_WARN, "tag", format, list);
+    esp_log_write(ESP_LOG_WARN, "tag", "\r\n");
     va_end(list);
 }
 
@@ -29,5 +32,6 @@ void app_esp_log_error_write(const char *format, ...)
     va_list list;
     va_start(list, format);
     esp_log_writev(ESP_LOG_ERROR, "tag", format, list);
+    esp_log_write(ESP_LOG_ERROR, "tag", "\r\n");
     va_end(list);
 }
